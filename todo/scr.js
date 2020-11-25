c7981f5840d1185be2404ed303727f54feba064e
BogdanNWJ <42960709+BogdanNWJ@users.noreply.github.com>
2020-11-25 11:44:05 +0100

    Update scr.js

--- todo/scr.js
+++ todo/scr.js
@@ -1,31 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const todoList = document.querySelector("#todoList");
-    const todoForm = document.querySelector("#todoForm");
-    const todoSearch = document.querySelector("#todoSearch");
-    const todoTextarea = todoForm.querySelector('textarea');
+const todoList = document.querySelector("#todoList");
+const todoForm = document.querySelector("#todoForm");
+const todoSearch = document.querySelector("#todoSearch");
+const todoTextarea = todoForm.querySelector('textarea');
 
-    
-});
-document.addEventListener("DOMContentLoaded", () => {
-    const todoList = document.querySelector("#todoList");
-    const todoForm = document.querySelector("#todoForm");
-    const todoSearch = document.querySelector("#todoSearch");
-    const todoTextarea = todoForm.querySelector('textarea');
+todoForm.addEventListener("submit", e => {
+    e.preventDefault();
 
-    function addTask(text) {
-        console.log("Dodaję zadanie do listy")
+    if (todoTextarea.value !== "") 
+    {
+        addTask(todoTextarea.value);
+        todoTextarea.value = "";
     }
-
-    todoForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        if (todoTextarea.value !== "") {
-            addTask(todoTextarea.value);
-            todoTextarea.value = "";
-        }
-    });
 });
+
 function addTask(text) {
+
     const element = document.createElement("div");
     element.classList.add("element");
 
@@ -35,23 +24,24 @@ function addTask(text) {
     //wrzucam do elementu
     element.append(elementInner);
 
-    //tworzę datę
-    const date = new Date();
-    const dateText = `${date.getDate()} - ${date.getMonth()+1} - ${date.getFullYear()} godz.: ${date.getHours()}:${date.getMinutes()}`;
-    element.querySelector(".element-date").innerText = dateText;
-
     //wstawiam tekst
     element.querySelector(".element-text").innerText = text;
 
     //i wrzucam element do listy
     todoList.append(element);
-}
-document.addEventListener("DOMContentLoaded", () => {
-    
 
-    todoList.addEventListener("click", e => {
-        if (e.target.classList.contains("element-delete")) {
-            e.target.closest(".element").remove();
-        }
+    //delete
+    const del = document.createElement("button");
+    del.innerText = "Usuń";
+    del.classList.add("delete");
+    del.addEventListener("click", e => {
+        e.target.parentElement.remove();
     });
-});
+
+    element.appendChild(del);
+
+    list.appendChild(element);
+    
+    console.log("Działa");
+}
+
