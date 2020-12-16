e80bbb167915f31f2c590ed815b9a118a99a8849
BogdanNWJ <42960709+BogdanNWJ@users.noreply.github.com>
2020-12-16 10:41:26 +0100

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -11,13 +11,13 @@
         <div class="head">
             <h1>Blog</h1>
         </div>
-        <div class="side">
+        //*<div class="side">
            <form action="wyszukiwanie.php" method="post" class='form_tag'>
                 <p>Wyszukaj tag:</p>
                 <p>#<input type='text' name='wyszukaj' class='szukaj'></p>
                 <input type="submit" class='szukaj' value='Wyszukaj'>
             </form>
-        </div>
+        </div>*//
         <div class="main">
             e
         </div>
